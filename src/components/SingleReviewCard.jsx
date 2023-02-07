718400f8a883d95fcd11dfc7eaa7fac63bb6c589
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-02-07 14:43:44 +0000

    fix some colour issues, plus adds a loading message

--- src/components/SingleReviewCard.jsx
+++ src/components/SingleReviewCard.jsx
@@ -1,17 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getReviewsByReviewId } from "../utils/api"
 
 const SingleReviewCard = ({review, setReview}) => {
     const {review_id} = useParams()
+    const [isLoading, setIsLoading] = useState(true)
     
     useEffect(() => {
+        setIsLoading(true)
         getReviewsByReviewId(review_id).then((reviewById) => {
             setReview(reviewById)
+            setIsLoading(false)
         })
     }, [review_id])
-    console.log(review_id, "<-- ID")
-    console.log(review, "<- Review")
+
+    if(isLoading) {
+        return <p>Review is Loading ...</p>
+    } else
+
     return (
         <section>
             <ul className="list">
@@ -20,6 +26,7 @@ const SingleReviewCard = ({review, setReview}) => {
                         <li id="review-img"><img src={review.review_img_url}></img></li>
                         <li className="single-review-footer" id='review-owner'>Review Author: {review.owner}</li>
                         <li className="single-review-footer" id="review-votes"> 👍 {review.votes} 👎</li>
+                        <li id="single-review-body"> {review.review_body} </li>
                         <li id="review-comments"> <h3>Comments:</h3> <h4>{review.comments}</h4></li>
                     
                     </ul>
