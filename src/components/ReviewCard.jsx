28a657ef21c8a689ec5b0186cd585aae4b7be42a
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-02-08 09:56:49 +0000

    add comments to single review cards, as well as an optional link to extended comment section

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -24,7 +24,7 @@ const ReviewCard = ({reviews, setReviews}) => {
                 {reviews.map((review) => {
             return <ul className="list" key={review.review_id}>
                         <li> <h2 id="review-title"> <Link to={`/reviews/${review.review_id}`}> {review.title} </Link> </h2> </li>
-                        <li id="review-img"><img src={review.review_img_url}></img></li>
+                        <li className="review-img"><img src={review.review_img_url} alt={review.title}></img></li>
                         <li id="review-votes"> <h3>Votes: </h3> <h4>{review.votes}</h4></li>
                         <li id="review-comments"> <h3>Comment Count: </h3> <h4>{review.comment_count}</h4></li>
                     
--- src/components/SingleReviewCard.jsx
+++ src/components/SingleReviewCard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams, Link } from "react-router-dom"
 import { getReviewsByReviewId } from "../utils/api"
+import Comments from './Comments'
 
 const SingleReviewCard = ({review, setReview}) => {
     const {review_id} = useParams()
@@ -24,11 +25,12 @@ const SingleReviewCard = ({review, setReview}) => {
             <ul className="list">
                         <li className="single-review-heading"> <h2 id="review-title"> {review.title}</h2> </li>
                         <li className="single-review-heading" id='game-designer'>Game Designer: {review.designer}</li>
-                        <li id="review-img"><img src={review.review_img_url}></img></li>
+                        <li className="review-img"><img src={review.review_img_url} alt={review.title}></img></li>
                         <li className="single-review-footer" id='review-owner'>Review Author: {review.owner}</li>
                         <li className="single-review-footer" id="review-votes"> 👍 {review.votes} 👎</li>
                         <li id="single-review-body"> {review.review_body} </li>
                         <li className="review-comment-count"> <h3 id='comment-count-heading'> <Link to={`/reviews/${review_id}/comments`}> Comment Section: </Link> </h3></li>
+                        <li><Comments /></li>
                     
                     </ul>
         </section>
