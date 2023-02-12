b8e7463a48646606099df9e9cf3cdde8a54ec706
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-02-12 12:54:05 +0000

    add mui styling to the single review card for native visuals

--- src/components/SingleReviewCard.jsx
+++ src/components/SingleReviewCard.jsx
@@ -3,6 +3,7 @@ import { useParams, Link } from "react-router-dom"
 import { getReviewsByReviewId } from "../utils/api"
 import Comments from './Comments'
 import ReviewVotes from "./ReviewVotes"
+import { Grid } from "@mui/material"
 
 const SingleReviewCard = ({review, setReview}) => {
     const {review_id} = useParams()
@@ -23,16 +24,25 @@ const SingleReviewCard = ({review, setReview}) => {
     return (
         <section>
             <ul className="list">
+                    <Grid 
+                    container-spacing={2}>
                         <li className="single-review-heading"> <h2 id="review-title"> {review.title}</h2> </li>
                         <li className="single-review-heading" id='game-designer'>Game Designer: {review.designer}</li>
+                    </Grid>
+                    <Grid>
                         <li className="review-img"><img src={review.review_img_url} alt={review.title}></img></li>
+                    </Grid>
+                    <Grid
+                    constainer-spacing={2}>
                         <li className="single-review-footer" id='review-owner'>Review Author: {review.owner}</li>
                         <li className="single-review-footer"><ReviewVotes votes={review.votes} review_id={review_id}/></li>
+                    </Grid>
                         <li id="single-review-body"> {review.review_body} </li>
                         <li className="review-comment-count"> <h3 id='comment-count-heading'> <Link to={`/reviews/${review_id}/comments`}> Comment Section: </Link> </h3></li>
                         <li><Comments /></li>
                     
                     </ul>
+                    
         </section>
     )
 }
